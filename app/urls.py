d7855d141563348be080ffc2968bdcd21d84b261
Ryan Choi <ryan.choi@gmail.com>
2014-05-03 23:21:08 -0700

    login and logout

--- app/urls.py
+++ app/urls.py
@@ -5,7 +5,10 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
 
-    url(r'^$', 'home.views.home', name='home'),
+    url(r'^$', 'home.views.login', name='login'),
+    url(r'^home$', 'home.views.home', name='home'),
+    url(r'^logout$', 'home.views.logout', name='logout'),
+
     url('', include('social.apps.django_app.urls', namespace='social')),
     url(r'^admin/', include(admin.site.urls)),
 )
--- home/views.py
+++ home/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import *
+from django.contrib.auth.decorators import login_required, user_passes_test
 
-# Create your views here.
+def login(request):
+    context = {"request": request}
+    return render_to_response('login.html', context, context_instance=RequestContext(request))
+
+@login_required
 def home(request):
-    context = {}
+    context = {"request": request}
     return render_to_response('home.html', context, context_instance=RequestContext(request))
+
+from django.contrib.auth import logout as auth_logout
+def logout(request):
+    """Logs out user"""
+    auth_logout(request)
+    return HttpResponseRedirect('/')
