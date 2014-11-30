9175477a2527d3ae96b396f32c004b9a362d2aaa
Gareth Paul Jones <gareth@garethpaul.com>
2014-11-29 17:13:23 -0800

    cleaning spacing #adhd

--- app/urls.py
+++ app/urls.py
@@ -4,11 +4,9 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-
     url(r'^$', 'home.views.login', name='login'),
     url(r'^home$', 'home.views.home', name='home'),
     url(r'^logout$', 'home.views.logout', name='logout'),
-
     url('', include('social.apps.django_app.urls', namespace='social')),
     url(r'^admin/', include(admin.site.urls)),
 )
