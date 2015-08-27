58e5be7ab005fd7196ec00c7f0371dc2fa3041dc
Ryan Choi <rchoi+github@twitter.com>
2015-08-26 22:15:36 -0700

    Update settings.py

--- app/settings.py
+++ app/settings.py
@@ -82,6 +82,14 @@ DATABASES = {
     'default': dj_database_url.config(default='sqlite://django-rest-apis.db')
 }
 
+# Uncomment for local database
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
