55258f18b25f442e514251209e1b1f8206ea41e4
Ryan Choi <ryan.choi@gmail.com>
2016-01-27 22:34:11 -0800

    urls for 1.9.1

--- app/urls.py
+++ app/urls.py
@@ -4,20 +4,21 @@ from django.contrib import admin
 admin.autodiscover()
 
 from app import settings
+from home import views
 
 urlpatterns = patterns('',
-    url(r'^$', 'home.views.login', name='login'),
-    url(r'^home$', 'home.views.home', name='home'),
-    url(r'^profile', 'home.views.profile', name='profile'),
-    url(r'^tweet', 'home.views.tweet', name='tweet'),
-    url(r'^query', 'home.views.query', name='query'),
-    url(r'^media/photo', 'home.views.media_photo', name='media_photo'),
-    url(r'^media/video', 'home.views.media_video', name='media_video'),
-    url(r'^media/inspector', 'home.views.media_inspector', name='media_inspector'),
-    url(r'^media', 'home.views.media_video', name='media_video'),
-    url(r'^logout$', 'home.views.logout', name='logout'),
+    url(r'^$', views.login, name='login'),
+    url(r'^home$', views.home, name='home'),
+    url(r'^profile', views.profile, name='profile'),
+    url(r'^tweet', views.tweet, name='tweet'),
+    url(r'^query', views.query, name='query'),
+    url(r'^media/photo', views.media_photo, name='media_photo'),
+    url(r'^media/video', views.media_video, name='media_video'),
+    url(r'^media/inspector', views.media_inspector, name='media_inspector'),
+    url(r'^media', views.media_video, name='media_video'),
+    url(r'^logout$', views.logout, name='logout'),
     url('', include('social.apps.django_app.urls', namespace='social')),
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', admin.site.urls),
 )
 
 urlpatterns += patterns('',
